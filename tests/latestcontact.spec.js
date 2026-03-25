bdabe7d50088aaa05608251d580bf12c222a9b93
mastsoke <raghavshrestha007@gmail.com>
2026-03-25 22:44:10 +0545

    Update latestcontact.spec.js

--- tests/latestcontact.spec.js
+++ tests/latestcontact.spec.js
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { LoginPage } from '../Page_Objects/login.po';
 import {ContactPage} from '../Page_Objects/contact.po';
 
-const testData = require('../fixtures/login.json');
+const testData = require('../Fixtures/login.json');
 const contactData = require('../fixtures/contact.json');
 const {authenticateUser, createEntity, deleteEntity, validateEntity, getEntity} = require ('../helper.spec.js');
 let accessToken;
